f06590530dca37857f3c09f57b202a5af0afce46
XuanPhat <phatpx32@wru.vn>
2024-05-29 22:01:59 -0400

    Setting routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Route } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+	{
+		path: 'category/:id',
+		component: ProductsComponent,
+	  },
+	  {
+		path: 'category',
+		component: ProductsComponent,
+	  },
+	  {
+		path: 'products',
+		component: ProductsComponent,
+	  },
+	  {
+		path: '',
+		redirectTo: '/products',
+		pathMatch: 'full',
+		// component: ProductsComponent,
+	  },
+	  {
+		path: '**',
+		redirectTo: '/products',
+		pathMatch: 'full',
+		// component: ProductsComponent,
+	  }
+];
